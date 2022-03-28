35f7952157a9604faa482760038a8afbadd13c3d
Estafany Ribeiro <69740553+EstafanyRibeiro@users.noreply.github.com>
2022-03-27 21:52:19 -0300

    Delete Calculadora.java

--- Calculadora.java
+++
@@ -1,6 +0,0 @@
-"java.configuration.runtimes": [
-        {
-            "name": "JavaSE-11",
-            "path": "/Library/Java/JavaVirtualMachines/sapmachine-jdk-11.0.6.0.1.jdk/Contents/Home"
-        }
-    ],
